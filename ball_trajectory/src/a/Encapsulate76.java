66a7555aef203cf07a0af5e4b896c4e6c8d3cb7d
Luís Delgado <lhds@cin.ufpe.br>
2016-06-06 21:40:09 -0300

    Começando a resolver problema a

---
+++ ball_trajectory/src/a/Encapsulate76.java
@@ -0,0 +1,11 @@
+package a;
+
+public class Encapsulate76 {
+	double x0, x1, x2, x3, x4;
+	double m = 1;
+	x1 = Fx (x0, x1, x2, x3, x4);
+	x0 = x1;			
+	public double Fx(double x0, double x1, double x2, double x3, double x4) {
+		return 100;
+	}
+}
